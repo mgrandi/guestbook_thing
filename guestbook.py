480e9dc63ffec8cf578d0d9787aaa787d2dc9479
weykent <weykent@weasyl.com>
2013-09-07 10:35:21 -0700

    Initial commit (it works!)

---
+++ guestbook.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, redirect, render_template, request
+
+
+app = Flask(__name__)
+entries = []
+entry_names = []
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html', entries=zip(entry_names, entries))
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    entries.append(request.form['entry'] or '<blank>')
+    if not request.form['name']:
+        flash("you must provide a name!")
+        return redirect('/')
+    entry_names.append(request.form['name'])
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
